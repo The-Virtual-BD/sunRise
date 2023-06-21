61451a3f44fcaa7edd1168fe4b89ac92532d2ab2
Hamid <www.abdulhamid3311@gmail.com>
2023-06-21 16:34:43 +0600

    Why page done

--- components/Pages/Why/Why.js
+++ components/Pages/Why/Why.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 import { BsArrowRight } from 'react-icons/bs';
 import { BenifitsData } from '../../sharedPage/StaticData';
-import { IoIosArrowForward } from "react-icons/io";
+import Form from '../../Homepage/Form';
 
 const Why = () => {
     return (
@@ -62,6 +62,7 @@ const Why = () => {
             <HowWeWork />
             <OurSpeciality />
             <Benifits />
+            <Form />
 
 
         </div>
@@ -86,13 +87,13 @@ const HowWeWork = () => {
                 <div className='mt-10 flex items-center justify-center'>
 
                     <div>
-                        <div className='py-10 px-10 bg-white w-[480px] '>
+                        <div className='py-10 px-10 bg-white max-w-[480px] '>
                             <h2 className='text-2xl font-bold mb-3'>Discover</h2>
                             <p className='text-sm lg:text-lg max-w-2xl'>We work to understand the goals and objectives for your project and organization. We then review the people, processes, and technologies used.</p>
                         </div>
 
 
-                        <div className='flex ml-4'>
+                        <div className='flex ml:2 lg:ml-4'>
                             <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1V55C1 60.5228 5.47715 65 11 65H65" stroke="#4DA687" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.5 5"></path></svg>
 
                             <div className='py-14 px-10 bg-white max-w-[480px] mt-7 '>
@@ -101,7 +102,7 @@ const HowWeWork = () => {
                             </div>
                         </div>
 
-                        <div className='flex ml-24'>
+                        <div className='flex ml-3 lg:ml-24'>
                             <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1V55C1 60.5228 5.47715 65 11 65H65" stroke="#4DA687" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.5 5"></path></svg>
 
                             <div className='py-14 px-10 bg-white max-w-[480px]  mt-7 '>
@@ -111,28 +112,28 @@ const HowWeWork = () => {
                         </div>
 
 
-                        <div className='flex ml-44'>
+                        <div className='flex ml-4 lg:ml-44'>
                             <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1V55C1 60.5228 5.47715 65 11 65H65" stroke="#4DA687" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.5 5"></path></svg>
 
-                            <div className='py-14 px-10 bg-white max-w-[480px]  mt-7 '>
+                            <div className='py-14 px-10 bg-white max-w-[480px]]  mt-7 '>
                                 <h2 className='text-2xl font-bold mb-3'>Iterate</h2>
                                 <p className='text-sm lg:text-lg max-w-2xl'>We gather feedback on what is working well and what can be improved, both within our teams and within your organization.</p>
                             </div>
                         </div>
 
-                        <div className='flex ml-64'>
+                        <div className='flex ml-5 lg:ml-64'>
                             <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1V55C1 60.5228 5.47715 65 11 65H65" stroke="#4DA687" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.5 5"></path></svg>
 
-                            <div className='py-14 px-10 bg-white w-[480px]  mt-7 '>
+                            <div className='py-14 px-10 bg-white max-w-[480px]  mt-7 '>
                                 <h2 className='text-2xl font-bold mb-3'>Scale</h2>
                                 <p className='text-sm lg:text-lg max-w-2xl'>For increased software delivery velocity, add dedicated teams as needed.</p>
                             </div>
                         </div>
 
-                        <div className='flex ml-96'>
+                        <div className='flex ml-6 lg:ml-96'>
                             <svg width="66" height="66" viewBox="0 0 66 66" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1V55C1 60.5228 5.47715 65 11 65H65" stroke="#4DA687" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="0.5 5"></path></svg>
 
-                            <div className='py-14 px-10 bg-white w-[480px]  mt-7 '>
+                            <div className='py-14 px-10 bg-white max-w-[480px]  mt-7 '>
                                 <h2 className='text-2xl font-bold mb-3'>Support</h2>
                                 <p className='text-sm lg:text-lg max-w-2xl'>We take great pride in our work, supporting the codebase and infrastructure behind systems we&apos;ve developed.</p>
                             </div>
@@ -149,12 +150,12 @@ const HowWeWork = () => {
 
 const OurSpeciality = () => {
     return (
-        <div className='bg-we-deifferent text-white py-24 flex items-center justify-center'>
+        <div className='bg-we-deifferent text-white py-14 lg:py-24 flex items-center justify-center'>
             <div className='max-w-7xl mx-auto relative'>
-                <h2 className='text-2xl lg:text-4xl font-extrabold pb-5'>How we&apos;re different from the competition</h2>
+                <h2 className='text-xl lg:text-4xl font-extrabold pb-5 px-5'>How we&apos;re different from the competition</h2>
 
                 <div className='bg-gradient-to-r from-[#466e94] to-[#3f8c67] p-10 rounded-md  relative'>
-                    <p className='text-xl font-semibold our-speciality-text-bg px-20 py-8'>When a consultancy and software development capability are under one roof, better decisions are made, maintenance costs are reduced, and software is delivered at a higher velocity.</p>
+                    <p className='text-sm lg:text-xl font-semibold our-speciality-text-bg px-5 lg:px-20 py-8'>When a consultancy and software development capability are under one roof, better decisions are made, maintenance costs are reduced, and software is delivered at a higher velocity.</p>
                 </div>
 
                 <div className='why-ceo-desc flex items-center justify-end gap-2 mt-4'>
@@ -176,26 +177,69 @@ const OurSpeciality = () => {
 
 const Benifits = () => {
     return (
-        <div className='bg-[#F0F4F7] text-darkBg px-5'>
-            <div className='max-w-7xl mx-auto py-10'>
-                <h2 className='text-2xl lg:text-5xl font-bold pb-5'>Benefits of working with us</h2>
+        <div className='bg-[#F0F4F7] text-darkBg '>
 
-                <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
-                    {
-                        BenifitsData.map(cardData => <div
-                            class={`card`}
-                            style={{ backgroundImage: `url(${cardData.img})` }}
-                            key={cardData.id}
-                        >
-                            <div class="content">
-                                <h2 class="title uppercase">{cardData.title}</h2>
-                                <p class="copy">{cardData.desc}</p>
-                            </div>
-                        </div>)
-                    }
+            <div className='max-w-7xl mx-auto py-10 px-5'>
+                <div>
+                    <h2 className='text-2xl lg:text-5xl font-bold pb-5'>Benefits of working with us</h2>
+
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                        {
+                            BenifitsData.map(cardData => <div
+                                class={`card`}
+                                style={{ backgroundImage: `url(${cardData.img})` }}
+                                key={cardData.id}
+                            >
+                                <div class="content">
+                                    <h2 class="title uppercase">{cardData.title}</h2>
+                                    <p class="copy">{cardData.desc}</p>
+                                </div>
+                            </div>)
+                        }
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className='bg-darkBg py-10 '>
+                    <h2 className='text-2xl lg:text-5xl font-bold  text-white max-w-7xl mx-auto px-5'>Our philosophy</h2>
+                </div>
+                <div className='bg-[#F8F9FB]  '>
+                    <div className='flex flex-col lg:flex-row items-center justify-between max-w-7xl mx-auto py-14'>
+                        <img src="/images/img-ikigai.svg" alt="" srcset="" />
+
+                        <div className='ikigai-container'>
+                            <p className='ikigai-text'>Ikigai happens when
+                                what you&apos;re good at
+                                overlaps with what you love and
+                                what the world needs</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='bg-white '>
+                <div className='max-w-7xl mx-auto py-14 px-5'>
+                    <h2 className='text-2xl lg:text-5xl font-bold pb-3'>High performance teams have high ikigai</h2>
+                    <h3 className='text-xl lg:text-3xl font-bold text-greenclr'>experienced people + love their work = productive</h3>
+
+                    <p className='text-sm lg:text-base my-5'>When you assemble a team with high ikigai, magic happens. Experienced people who love their work are productive, tasks are done with high fidelity, and decisions are made with care.</p>
+
+                    <p className='text-sm lg:text-base'>At SiliconMint, this is the foundation of how we assemble engineering teams: people who are deeply technical, love what they do, and have a passion for the craft of programming itself.</p>
                 </div>
 
+            </div>
 
+            <div className='bg-we-deifferent text-white py-24 flex items-center justify-center'>
+                <div className='max-w-7xl mx-auto relative'>
+                    <h2 className='text-center text-xl mb-2'>Stay small, stay hungry</h2>
+
+                    <div className=' ikigai-container'>
+                        <p className=' ikigai-text'><span className='text-greenclr'>You don&apos;t need a large team </span> to accomplish great things. Conversely, great things are accomplished by small teams.</p>
+                    </div>
+
+                </div>
 
             </div>
         </div>
