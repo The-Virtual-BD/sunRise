0515fa33088102c882b876da9cd0b6d80aa2fa05
Hamid <www.abdulhamid3311@gmail.com>
2023-06-24 14:59:49 +0600

    about us done

--- components/Pages/company/About.js
+++ components/Pages/company/About.js
@@ -3,15 +3,16 @@ import Brand from '../../Homepage/Brand';
 import Feedbacks from '../Services/Feedbacks';
 import Team from '../../Homepage/Team';
 import Blogs from '../../Homepage/Blogs';
+import Image from 'next/image';
 
 const About = () => {
     return (
         <div>
             <AboutBanner />
             <AboutMission />
-
             <Team />
             <Feedbacks />
+            <AboutHistory />
             <Blogs />
 
         </div>
@@ -50,10 +51,17 @@ const AboutMission = () => {
     return (
         <div className='bg-[#F8FAFB] text-darkBg '>
 
-            <div className='max-w-7xl mx-auto px-5 lg:px-0 flex items-center gap-3'>
+            <div className='max-w-7xl mx-auto px-5 lg:px-0 flex flex-col lg:flex-row items-end gap-3 pt-10'>
                 <div className='w-full lg:w-1/2'>
-                    <h2 className='text-2xl lg:text-4xl font-bold pb-3'>OUR MISSION</h2>
-                    <p className='text-sm lg:text-base my-5'>We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers... We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers....We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers</p>
+                    <div>
+                        <h2 className='text-2xl lg:text-4xl font-bold pb-3'>Our Mission</h2>
+                        <p className='text-sm lg:text-base my-3'>We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers... We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers....We are committed to providing state-of-the-art technological solutions to our valued customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers</p>
+                    </div>
+
+                    <div className='mt-5'>
+                        <h2 className='text-2xl lg:text-4xl font-bold pb-3 '>Our Vision</h2>
+                        <p className='text-sm lg:text-base my-3'>We are committed to providing state-of-the-art technology solutions to our customers; to meet and exceed their operational targets and budgetary expectations, therefore creating financial growth and opportunities for our customers.</p>
+                    </div>
                 </div>
 
                 <div className='w-full lg:w-1/2'>
@@ -70,18 +78,27 @@ const AboutMission = () => {
 
 
 
-/* 
-const AboutBanner=()=>{
-    return(
-        <div>
+
+const AboutHistory = () => {
+    return (
+        <div className='  bg-white p-5 lg:p-10 py-20'>
+
+            <div className='bg-[#F8FAFB] text-darkBg  max-w-7xl mx-auto px-5 lg:px-0 shadow-xl'>
+                <div className='p-3 lg:p-14 flex flex-col lg:flex-row gap-5'>
+                    <div className='w-full lg:w-1/2'>
+                        <h2 className='text-2xl lg:text-4xl font-bold pb-3 '>Our History</h2>
+                        <p className='text-sm lg:text-base my-3'>Established in 2018 SunRise Communications (SunCom) has dedicated itself to becoming the #1 service providers in advance ROIP Two-Way Communications services and Biometrics Systems. Are passion is technology and we encourage you our future clients to “Work Smarter, Not Harder”. In the age of smart technology, and the ‘internet of things’, efficiency improvements and cost savings are waiting to be found. Communications and Technology opens up a world of efficiency boosting applications for your business. When valuable information is uncovered, the goal is to share it instantly to all concerned parties, no matter where they are located. Knowledge is power. Help your organization and ultimately your teams, reach their full potential by unifying your internal communications and technology. Your solution is just one push away.</p>
+                    </div>
+
+                    <div className='w-full lg:w-1/2 flex items-center gap-2 justify-center'>
+                        <Image src="/images/about/TTC.jpg" width={265} height={353} alt="" srcset="" />
+                        <Image src="/images/about/TTC-2.jpg" width={265} height={353} className='hidden lg:block' alt="" srcset="" />
+
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
 };
-const AboutBanner=()=>{
-    return(
-        <div>
-
-        </div>
-    )
-}; */
